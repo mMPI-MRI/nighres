1e25a176ce1cc5e65ab0d9b90dfc53010bbb84f6
Chris Steele <steelec@gmail.com>
2017-10-03 15:57:26 -0400

    fixes

--- nighres/__init__.py
+++ nighres/__init__.py
@@ -3,6 +3,7 @@ import brain
 import laminar
 import surface
 import data
+import volume
 from global_settings import ATLAS_DIR, TOPOLOGY_LUT_DIR, DEFAULT_ATLAS
 
-__all__ = ['io', 'brain', 'laminar', 'surface', 'data', '__version__']
+__all__ = ['io', 'brain', 'laminar', 'surface', 'data', 'volume', '__version__']
--- nighres/volume/voxel_comparisons.py
+++ nighres/volume/voxel_comparisons.py
@@ -2,7 +2,7 @@ import numpy as np
 import nibabel as nb
 import os
 import sys
-import cbstools
+#import cbstools
 from ..io import load_volume, save_volume
 from ..utils import _output_dir_4saving
 
@@ -26,11 +26,11 @@ def _volume_to_1d(volume_file, mask=None):
 	else:
 		return data.flatten()
 
-def image_pair(contrast_image1, contrast_image2, mask_file=None, distances=['euclidian','seuclidian','correlation'], return_data_vectors=True, remove_zero_voxels=False):
+def image_pair(contrast_image1, contrast_image2, mask_file=None, return_data_vectors=True, remove_zero_voxels=False):
 	'''
 	Directly compare the voxels of one contrast image to another.
 	Intended to be used for comparing two different contrasts in a
-	single subject.
+	single subject. Returns correlation and data vectors as requested
 
 	Images MUST be co-registered and of the same space, affine's are not
 	checked and no up/down-sampling is performed
@@ -41,16 +41,6 @@ def image_pair(contrast_image1, contrast_image2, mask_file=None, distances=['euc
 		Second input image
 	mask_file: niimg
 		Mask {0,1} of the same dimensions as input contrast_image1/2
-	distance_metrics: str|list
-		Single string or list of distances to conduct on input images
-		Can be 'all' or one or more of the distance metrics from
-		scipy.spatial.distance.cdist:
-		'braycurtis', 'canberra', 'chebyshev', 'cityblock',
-		'correlation', 'cosine', 'dice', 'euclidean', 'hamming',
-		'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski',
-		'rogerstanimoto', 'russellrao', 'seuclidean',
-		'sokalmichener', 'sokalsneath', 'sqeuclidean', 'wminkowski',
-		'yule'.
 	return_data_vectors: bool (default = True)
 		Return 1d data vectors of input image in dictionary as
 		"input_vectors"
@@ -60,11 +50,8 @@ def image_pair(contrast_image1, contrast_image2, mask_file=None, distances=['euc
 		either of your datasets (i.e., it corresponds to non-data), or
 		if you are too lazy to provide a mask and want something quick!
 	'''
-
-	import scipy.spatial.distance.cdist as cdist
-
-	v1 = _volume_to_1d(contrast_image1, mask_file=mask_file)
-	v2 = _volume_to_1d(contrast_image2, mask_file=mask_file)
+	v1 = _volume_to_1d(contrast_image1, mask=mask_file)
+	v2 = _volume_to_1d(contrast_image2, mask=mask_file)
 
 	# if we are sure that 0 is meaningless and not to be compared,
 	# remove from both vectors
@@ -85,19 +72,6 @@ def image_pair(contrast_image1, contrast_image2, mask_file=None, distances=['euc
 	if return_data_vectors:
 		res["input_vectors"] = np.vstack([v1,v2])
 
-	if distance_metrics is not None:
-		if isinstance(distance_metrics,basestring):
-			distance_metrics = [distance_metrics]
-		if 'all' in distance_metrics:
-			distance_metrics = ['braycurtis', 'canberra', 'chebyshev', 'cityblock',
-			'correlation', 'cosine', 'dice', 'euclidean', 'hamming',
-			'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski',
-			'rogerstanimoto', 'russellrao', 'seuclidean',
-			'sokalmichener', 'sokalsneath', 'sqeuclidean', 'wminkowski',
-			'yule']
-		for metric in distance_metrics:
-			# res["euclidian_distance"] = np.linalg.norm(v1-v2,2,0)
-			res[metric] = cdist(v1,v2,metric=metric)
 	return res
 
 def extract_data_multi_image(contrast_image_list, mask_file=None, image_zero_value=None):
@@ -106,9 +80,9 @@ def extract_data_multi_image(contrast_image_list, mask_file=None, image_zero_val
 	mask_file contains more than one non-zero value, output will be grouped
 	in columns according to sorted (increasing) mask_ids. Column indices provided
 	in output as mask_id_start_stop
-	
+
 	image_zero_value: int
-		This value is used to construct a mask from the first image in 
+		This value is used to construct a mask from the first image in
 		contrast_image_list (this value is set to 0, i.e., masked out)
 		ONLY used when mask_file is not provided. Don't be lazy, provide
 		a mask if you can.
@@ -124,12 +98,18 @@ def extract_data_multi_image(contrast_image_list, mask_file=None, image_zero_val
 		mask_data = np.ones_like(load_volume(contrast_image_list[0]).shape)
 		if image_zero_value is not None:
 			mask_data[load_volume(contrast_image_list[0]).get_data() == image_zero_value] = 0
-	
+
 	mask_ids = np.unique(mask_data)
 	mask_ids.sort()
 
 	# pre=allocate an array for data
 	# rows are contrast images, cols are the data from each of the segs
+	# fastest way to do this, but obviously not the most straight forward
+	# to work with later - this is damn fast though
+	# TODO: make easier to use/interpret (lists of arrays)
+	# # mod: create an empty list of lists and then fill with arrays of known size
+	# # mod: res_list = [[] for _ in range(np.sum(mask_ids>0))]
+
 	res = np.zeros((len(contrast_image_list),np.sum(mask_data.flatten()>0)))*np.nan
 	mask_id_start_stop  = np.zeros((np.sum(mask_ids>0),3))*np.nan
 
@@ -137,16 +117,19 @@ def extract_data_multi_image(contrast_image_list, mask_file=None, image_zero_val
 		start = 0
 
 		for mask_id_idx, mask_id in enumerate(mask_ids[mask_ids > 0]):
+			print("mask id: {}".format(mask_id))
 			seg_mask = np.zeros_like(mask_data)
 			seg_mask[mask_data==mask_id] = 1
 			seg_vec = _volume_to_1d(contrast_image, mask=seg_mask)
+			print(seg_vec.shape)
 			stop = len(seg_vec)+start
+			print("{} - {}".format(start,stop))
 			res[image_idx,start:stop] = seg_vec
-			start = stop + 1
 
 			# construct a lookup for the mask ids and their respective start
 			# and stop columns, but only on the first image since they will
 			# all be the same
 			if image_idx == 0:
 				mask_id_start_stop[mask_id_idx] = np.array([mask_id,start,stop])
-	return res, mask_id_start_stop
+			start = np.copy(stop)
+	return res, mask_id_start_stop.astype(int)
